a3169b621d4b63744c5d438228051da0aa6153c7
joanneBlocks <jscosto@gmail.com>
2026-04-06 07:09:51 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,29 @@ router.get('/', async (req, res) => {
       include: { author: true },
       orderBy: { createdAt: 'desc' }
     })
-    res.render('index', { messages })
+
+    // Never send passwords to the frontend
+    const safeMessages = messages.map(msg => ({
+      id: msg.id,
+      title: msg.title,
+      text: msg.text,
+      createdAt: msg.createdAt,
+      author: {
+        firstName: msg.author.firstName,
+        lastName: msg.author.lastName
+      }
+    }))
+
+    // Sanitize currentUser too
+    const safeUser = req.user ? {
+      id: req.user.id,
+      firstName: req.user.firstName,
+      lastName: req.user.lastName,
+      isMember: req.user.isMember,
+      isAdmin: req.user.isAdmin
+    } : null
+
+    res.render('index', { messages: safeMessages, safeUser })
   } catch (err) {
     console.error(err)
     res.status(500).send('Something went wrong')
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'public/js',
+    rollupOptions: {
+      input: {
+        messageBoard: 'src/messageBoard.jsx'
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
+})
