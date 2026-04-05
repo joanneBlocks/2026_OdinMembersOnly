387bd77752595aa1b48930aeeab97174e4f4a6a3
joanneBlocks <jscosto@gmail.com>
2026-04-06 06:13:19 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { PrismaClient } = require('@prisma/client')
-
-const prisma = new PrismaClient()
+const prisma = require('../db')
 
 router.get('/', async (req, res) => {
   try {
