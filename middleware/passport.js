3267ee1b01d58a116fa957babaf5b4ba07cd72fc
joanneBlocks <jscosto@gmail.com>
2026-04-06 06:13:04 +0800

    Update passport.js

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,9 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcryptjs')
-const { PrismaClient } = require('@prisma/client')
-
-const prisma = new PrismaClient()
+const prisma = require('../db')
 
 passport.use(new LocalStrategy(
   { usernameField: 'email' },
