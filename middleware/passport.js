d8e95c94fac48bbe7a5bbbdea86399fa7c13c545
joanneBlocks <jscosto@gmail.com>
2026-04-06 05:48:43 +0800

    Update passport.js

---
+++ middleware/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+passport.use(new LocalStrategy(
+  { usernameField: 'email' },
+  async (email, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({ where: { email } })
+      if (!user) return done(null, false, { message: 'No user found' })
+
+      const match = await bcrypt.compare(password, user.password)
+      if (!match) return done(null, false, { message: 'Wrong password' })
+
+      return done(null, user)
+    } catch (err) {
+      return done(err)
+    }
+  }
+))
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findUnique({ where: { id } })
+    done(null, user)
+  } catch (err) {
+    done(err)
+  }
+})
+
+module.exports = passport
