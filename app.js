ce05db2f8110e55ef926873f45ca47e3fc0b9b1f
joanneBlocks <jscosto@gmail.com>
2026-04-06 08:13:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -52,4 +52,5 @@ app.use('/', authRouter)
 app.use('/', clubRouter)
 app.use('/', messagesRouter)
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
